d4c44b9ecdf54fe936a3406b4413101cc2e22809
Ditz Guntlisbergen <itsditz33@gmail.com>
2022-05-02 20:13:34 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {},
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
